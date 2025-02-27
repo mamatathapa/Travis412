48cd850b5f2146e53fd67b3f84cd30e535275f79
Mamata Thapa <103456038+mamatathapa@users.noreply.github.com>
2025-02-27 16:49:31 -0500

    Update SimpleCalculator.java

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -4,4 +4,20 @@ public class SimpleCalculator {
 	public int add(int a, int b) {
 		return a + b;
 	}
+	public int minus(int a, int b) {
+		return a - b;
+	}	
+	
+	
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+	
+	public int divide(int a, int b) {
+		if (b==0) {
+			System.out.println("Error: Division by zero is not allowed.");
+        return 0;
+    }
+		return a/b;
+	}
 }
